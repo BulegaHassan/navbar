adcb19c13998d45b476b48bcb24da1baa42dab98
Hassan Bulega <hassanbulega@gmail.com>
2022-06-29 17:49:52 +0300

    refactor: show/hide classes to toggle nav button

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {
-          showLinks && 
-        <div className='links-container show-container'>
+        
+         
+        <div className={`${showLinks ? 'links-container show-container':'links-container'}`}>
           <ul className='links'>
             {links.map((link) => {
               const { id, url, text } = link;
@@ -28,7 +28,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
-        }
+        
         <ul className='social-icons'>
           {social.map((socialIcon) => {
             const { id, url, icon } = socialIcon;
