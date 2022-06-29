1d6acc5be1b077d5997dbd3295fc8c38eea148fa
Hassan Bulega <hassanbulega@gmail.com>
2022-06-29 17:16:06 +0300

    feat: add logo and navbar items

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,54 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   return (
-   
-  )
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container show-container'>
+          <ul className='links'>
+            <li>
+              <a href='#'>home</a>
+            </li>
+            <li>
+              <a href='#'>about</a>
+            </li>
+            <li>
+              <a href='#'>contact</a>
+            </li>
+            <li>
+              <a href='#'>products</a>
+            </li>
+            <li>
+              <a href='#'>home</a>
+            </li>
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          <li>
+            <a href='https://twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com'>
+              <FaTwitter />
+            </a>
+          </li><li><a href="https://twitter.com"><FaTwitter/></a></li>
+          
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
